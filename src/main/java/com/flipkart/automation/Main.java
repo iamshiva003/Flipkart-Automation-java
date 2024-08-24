8358d8433fdbb4c4389a75a0d630437c1110ef50
iamshiva003 <shivu9887@gmail.com>
2024-08-24 16:20:47 +0530

    Got the WebElemets

--- src/main/java/com/flipkart/automation/Main.java
+++ src/main/java/com/flipkart/automation/Main.java
@@ -5,10 +5,20 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.SQLOutput;
+import java.time.Duration;
 import java.util.List;
 
 public class Main {
+
+    public static void waitABit(WebDriver driver, WebElement nextPage) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(d -> nextPage.isDisplayed());
+    }
+
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
@@ -27,7 +37,16 @@ public class Main {
 
         for (int i = 0; i < totalPages; i++) {
             List<WebElement> productNames = driver.findElements(By.xpath("//div[@class=\"KzDlHZ\"]"));
-
+            List<WebElement> productPrices = driver.findElements(By.xpath("//div[@class=\"Nx9bqj _4b5DiR\"]"));
+            List<WebElement> productOverallRatings = driver.findElements(By.xpath("//div[@class=\"XQDdHH\"]"));
+            List<WebElement> productRatings = driver.findElements(By.xpath("//span[contains(text(),\"Ratings\")]"));
+            List<WebElement> productReviews = driver.findElements(By.xpath("//span[contains(text(),\"Reviews\")]"));
+            System.out.println(productNames.size());
+            for (int j = 0; j < productNames.size(); j++) {
+                System.out.println(productNames.get(j).getText());
+            }
+            waitABit(driver, nextPage);
+            nextPage.click();
         }
 
         driver.quit();
